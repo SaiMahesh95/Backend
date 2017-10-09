b35387980d27adde78eead322c71062ac3b81aa9
MAHESH <MAHESH@192.168.0.4>
2017-10-09 09:41:12 +0530

    error

--- src/main/java/com/collaborate/service/BlogPostService.java
+++ src/main/java/com/collaborate/service/BlogPostService.java
@@ -1,5 +1,12 @@
 package com.collaborate.service;
 
-public class BlogPostService {
+import java.util.List;
+
+import com.collaborate.Model.BlogPost;
+
+public interface BlogPostService {
+	void addBlogPost(BlogPost blogPost);
+	List<BlogPost> getBlogs(int approved);
+	BlogPost getBlogById(int id);
 
 }
